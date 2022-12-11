d30087178c6f4cb23fe51d904a2736063862c485
bessonniy11 <50315006+bessonniy11@users.noreply.github.com>
2022-12-11 23:58:51 +0300

    6_recipes_data_v4

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://ibessonniy.ru/api/vendor/',
+  publicUrl: 'https://ibessonniy.ru/api/vendor/',
+  apiUrl: 'mobile/',
+  newApiUrl: 'mobile/api/v1/',
+  imagesUrl: 'https://ibessonniy.ru/api/images/',
+  dishesImagesUrl: 'https://ibessonniy.ru/api/dishesImages/',
 };
